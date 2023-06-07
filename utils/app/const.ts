4d88390d19a2ed01bd3b670d1d6d75f8ab424956
synalocey <32924919+synalocey@users.noreply.github.com>
2023-06-07 09:45:47 +0800

    Update const.ts

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -19,3 +19,6 @@ export const OPENAI_ORGANIZATION =
 
 export const AZURE_DEPLOYMENT_ID =
   process.env.AZURE_DEPLOYMENT_ID || '';
+
+export const LLM_REPORT_API_KEY =
+  process.env.LLM_REPORT_API_KEY || '';
